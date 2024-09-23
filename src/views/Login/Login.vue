4df7af6f54007d8ab48fd79e908e60d73864576a
jarrett <895152506@qq.com>
2024-09-23 11:19:52 +0800

    9/23-1

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,83 +1,97 @@
 <template>
   <div id="body">
     <div class="main">
-      <div class="title">
-        <strong>{{ Login_title }}</strong>
+      <div class="QR-CODE" @click="QDLogin">
+        <div class="img-qr"></div>
       </div>
-      <div class="button_control">
-        <el-button type="primary" circle>
-          <el-icon><Edit /></el-icon>
-        </el-button>
-        <el-button type="success" circle>
-          <el-icon><Check /></el-icon>
-        </el-button>
-        <el-button type="info" circle>
-          <el-icon><Message /></el-icon>
-        </el-button>
-      </div>
-      <div class="input-1">
-        <el-tag
-          style="
-            position: relative;
-            right: 22px;
-            font-size: 14px;
-            color: purple;
-          "
-          ><strong>用户名</strong></el-tag
-        >
-        <el-input
-          v-model="Account_inf.Username"
-          style="width: 240px"
-          placeholder="Usernasme"
-          v-on:keyup.enter="enterLogin"
-          prefix-icon="UserFilled"
-        />
-      </div>
-      <div class="input-2">
-        <el-tag
-          style="
-            position: relative;
-            right: 22px;
-            font-size: 14px;
-            color: purple;
-          "
-          ><strong>密码</strong></el-tag
-        >
-        <el-input
-          v-model="Account_inf.password"
-          style="width: 240px; left: 7px"
-          placeholder="Password"
-          v-on:keyup.enter="enterLogin"
-          type="password"
-          :show-password="true"
-          prefix-icon="Lock"
-        />
-      </div>
-      <div class="button-forget">
-        <el-button
-          plain
-          text
-          style="font-size: 12px; padding: 8px"
-          class="forget_password"
-          ><strong>忘记密码?</strong></el-button
-        >
-        <el-checkbox
-          v-model="checked"
-          label="是否保持登录状态"
-          style="font-size: 10px; left: 40px; font-weight: bolder"
-        />
-        <!-- font-weight：设置字体样式/bolder：加粗 -->
-      </div>
-      <div class="button-login">
-        <el-button
-          type="primary"
-          round
-          style="width: 180px"
-          class="login"
-          @click="enterLogin"
-          >{{ LoginIn }}</el-button
-        >
-      </div>
+      <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick" style="position: relative;top: -65px;">
+        <el-tab-pane label="账号密码登录" name="first" >
+          <div class="title">
+            <strong>{{ Login_title }}</strong>
+          </div>
+          <div class="Line-text">
+            <label style="font-size: 12px;position: relative;top: 290px;left: 70px;color: grey;"><strong>———————— 第三方登录 ————————</strong></label>
+            </div>
+          <div class="button_control">
+            <el-button type="primary" circle>
+              <el-icon><Edit /></el-icon>
+            </el-button>
+            <el-button type="success" circle>
+              <el-icon><Check /></el-icon>
+            </el-button>
+            <el-button type="info" circle>
+              <el-icon><Message /></el-icon>
+            </el-button>
+          </div>
+          <div class="input-1">
+            <el-tag
+              style="
+                position: relative;
+                right: 22px;
+                font-size: 14px;
+                color: purple;
+              "
+              ><strong>用户名</strong></el-tag
+            >
+            <el-input
+              v-model="Account_inf.Username"
+              style="width: 240px"
+              placeholder="Usernasme"
+              v-on:keyup.enter="enterLogin"
+              prefix-icon="UserFilled"
+            />
+          </div>
+          <div class="input-2">
+            <el-tag
+              style="
+                position: relative;
+                right: 22px;
+                font-size: 14px;
+                color: purple;
+              "
+              ><strong>密码</strong></el-tag
+            >
+            <el-input
+              v-model="Account_inf.password"
+              style="width: 240px; left: 7px"
+              placeholder="Password"
+              v-on:keyup.enter="enterLogin"
+              type="password"
+              :show-password="true"
+              prefix-icon="Lock"
+            />
+          </div>
+          <div class="button-forget">
+            <el-button
+              plain
+              text
+              style="font-size: 12px; padding: 8px"
+              class="forget_password"
+              ><strong>忘记密码?</strong></el-button
+            >
+            <el-checkbox
+              v-model="checked"
+              label="是否保持登录状态"
+              style="font-size: 10px; left: 40px; font-weight: bolder"
+            />
+            <!-- font-weight：设置字体样式/bolder：加粗 -->
+          </div>
+          <div class="button-login">
+            <el-button
+              type="primary"
+              round
+              style="width: 180px"
+              class="login"
+              @click="enterLogin"
+              >{{ LoginIn }}</el-button
+            >
+          </div>
+        </el-tab-pane>
+        <el-tab-pane label="手机验证登录" name="second">
+        </el-tab-pane>
+        <el-tab-pane label="二维码登录" name="third" v-if="QRAllow">
+        </el-tab-pane>
+      </el-tabs>
     </div>
   </div>
 </template>
@@ -92,7 +106,7 @@ import {
   Lock,
 } from "@element-plus/icons-vue";
 import { ElMessage } from "element-plus";
-import { ref, reactive,provide } from "vue";
+import { ref, reactive} from "vue";
 import { useRouter } from "vue-router";
 // import axios from 'axios'
 // import $ from 'jquery'
@@ -122,6 +136,7 @@ export default {
     });
     const LoginIn = ref("Login In");
     const router = useRouter();
+    const activeName = ref('first')
 
     //登录事件  ログインイベント
     const enterLogin = async () => {
@@ -138,7 +153,10 @@ export default {
             type: "success",
           });
           //路由跳转,带出登录的工号信息
-          router.push({path:"/HomePage",query: { UserNum_only: Account_inf.Username } });
+          router.push({
+            path: "/HomePage",
+            query: { UserNum_only: Account_inf.Username },
+          });
         } else {
           ElMessage({
             message: "login fail, please try again",
@@ -151,12 +169,22 @@ export default {
       }
     };
 
+    //扫码登录
+    const QRAllow = ref(false)
+    const QDLogin=()=>{
+      activeName.value = ref('third')
+    }
+
+
     return {
       Account_inf,
       checked,
       enterLogin,
       Login_title,
       LoginIn,
+      activeName,
+      QDLogin,
+      QRAllow,
     };
   },
 };
