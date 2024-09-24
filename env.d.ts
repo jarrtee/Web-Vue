60fdecfee02d0949e232df8bf0a726c1a63e34a2
jarrett <895152506@qq.com>
2024-09-24 15:27:35 +0800

    9/24-1

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+//解决any隐式,找不到类型
+declare module "*.vue"{
+    import {Component} from "vue";
+    const component: Component;
+    export default component;
+}
