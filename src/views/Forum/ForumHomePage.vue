7834fc7a0f480badedea3098cb3c00f907902715
jarrett <895152506@qq.com>
2024-09-25 15:26:52 +0800

    9/25-2

--- src/views/Forum/ForumHomePage.vue
+++ src/views/Forum/ForumHomePage.vue
@@ -1,6 +1,8 @@
 <template>
-    <div :style="{ background: 'rgb(180, 200, 222)', padding: '16px 16px' }" >
-        
+    <div :style="{ background: 'rgb(180, 200, 222)', padding: '16px 16px' }" class="topEara">
+    </div>
+    <div :style="{ background: 'rgb(180, 20, 222)', padding: '16px 16px' ,height:'1200px'}" >
+        <h2>as</h2>
     </div>
 </template>
   
@@ -13,4 +15,10 @@ export default {
 
 <style lang="less" scoped>
 
+.topEara{
+    position: -webkit-sticky;
+    position: sticky;
+    z-index: 1000;
+    top: 0px;
+}
 </style>
