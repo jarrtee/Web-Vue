83e5b2e1eef904a3db6cfdf0b19ca72b88c1a40b
jarrett <895152506@qq.com>
2024-10-30 15:09:24 +0800

    10/30

--- src/views/Forum/ForumHomePage.vue
+++ src/views/Forum/ForumHomePage.vue
@@ -1,13 +1,85 @@
 <template>
   <div
     :style="{
-      background: 'rgb(180, 200, 222)',
-      padding: '11px 16px',
-      height: '25px',
+      background: 'rgb(79,79,79)',
+      padding: '8px 10px',
+      height: '60px',
+      opacity: 0.95,
     }"
     class="topEara"
   >
-    <span>DDDDDDDDDDDDDD</span>
+    <el-button
+      text
+      plain
+      class="HomePageButton"
+      @click="HomePageClick"
+      :style="{
+        border: HomePageAppear ? '2px solid white' : 'none',
+        color :HomePageAppear? 'rgb(127,205,0)':'white'
+      }"
+      >首页
+      <el-icon v-if="HomePageAppear == false" style="left: 2px;">
+        <ArrowDown/>
+      </el-icon>
+      <el-icon v-if="HomePageAppear == true" style="left: 2px;">
+        <ArrowUp/>
+      </el-icon>
+      </el-button
+    >
+    <el-button
+      text
+      plain
+      class="InformationButton"
+      @click="InformationClick"
+      :style="{
+        border: InformationAppear ? '2px solid white' : 'none',
+        color :InformationAppear? 'rgb(127,205,0)':'white'
+      }"
+      >资讯
+      <el-icon v-if="InformationAppear == false" style="left: 2px;">
+        <ArrowDown/>
+      </el-icon>
+      <el-icon v-if="InformationAppear == true" style="left: 2px;">
+        <ArrowUp/>
+      </el-icon>
+      </el-button
+    >
+    <el-button
+      text
+      plain
+      class="CommunityButton"
+      @click="CommunityClick"
+      :style="{
+        border: CommunityAppear ? '2px solid white' : 'none',
+        color :CommunityAppear? 'rgb(127,205,0)':'white'
+      }"
+      >社区
+      <el-icon v-if="CommunityAppear == false" style="left: 2px;">
+        <ArrowDown/>
+      </el-icon>
+      <el-icon v-if="CommunityAppear == true" style="left: 2px;">
+        <ArrowUp/>
+      </el-icon>
+      </el-button
+    >
+    <el-button
+      text
+      plain
+      class="SupportButton"
+      @click="SupportClick"
+      :style="{
+        border: SupportAppear ? '2px solid white' : 'none',
+        color :SupportAppear? 'rgb(127,205,0)':'white'
+      }"
+      >支持
+      <el-icon v-if="SupportAppear == false" style="left: 2px;">
+        <ArrowDown/>
+      </el-icon>
+      <el-icon v-if="SupportAppear == true" style="left: 2px;">
+        <ArrowUp/>
+      </el-icon>
+      </el-button
+    >
   </div>
   <div
     :style="{
@@ -16,16 +88,22 @@
       height: '1200px',
     }"
   >
-    <h2>position:{{ x }},{{ y }}</h2>
+    <span>实时位置检测</span>
+    <span>position:{{ x }},{{ y }}</span>
   </div>
 </template>
 
 <script>
-import { ref, onMounted, onUnmounted } from "vue";
+import {ArrowDown,ArrowUp} from "@element-plus/icons-vue";
+import { ref, onMounted, onUnmounted, watchEffect } from "vue";
 // import { useEventListener } from "./event";
 export default {
   data() {
     return {};
+  },
+  components:{
+    ArrowDown,
+    ArrowUp
   },
   setup() {
     //鼠标追踪,获取坐标值
@@ -45,29 +123,59 @@ export default {
       onMounted(() => window.addEventListener("mousemove", update));
       onUnmounted(() => window.removeEventListener("mousemove", update));
 
-    //useEventListener简化组合函数
-    //   useEventListener(window, "mousemove", (event) => {
-    //     x.value = event.pageX;
-    //     y.value = event.pageY;
-    //   });
+      //useEventListener简化组合函数
+      //   useEventListener(window, "mousemove", (event) => {
+      //     x.value = event.pageX;
+      //     y.value = event.pageY;
+      //   });
 
       // 通过返回值暴露所管理的状态
       return { x, y };
     };
     const { x, y } = useMouse();
+
+    //首页按钮
+    const HomePageAppear = ref(false);
+    const HomePageClick = () => {
+      HomePageAppear.value = !HomePageAppear.value;
+    };
+
+    //资讯按钮
+    const InformationAppear = ref(false);
+    const InformationClick = () => {
+      InformationAppear.value = !InformationAppear.value;
+    };
+
+    //社区按钮
+    const CommunityAppear = ref(false);
+    const CommunityClick = () => {
+      CommunityAppear.value = !CommunityAppear.value;
+    };
+
+    //支持按钮
+    const SupportAppear = ref(false);
+    const SupportClick = () => {
+      SupportAppear.value = !SupportAppear.value;
+    };
+
+    // 实时监控
+    watchEffect(() => {});
     return {
       x,
       y,
+      HomePageClick,
+      HomePageAppear,
+      InformationClick,
+      InformationAppear,
+      CommunityAppear,
+      CommunityClick,
+      SupportAppear,
+      SupportClick
     };
   },
 };
 </script>
 
 <style lang="less" scoped>
-.topEara {
-  position: -webkit-sticky;
-  position: sticky;
-  z-index: 1000;
-  top: 0px;
-}
+@import url("../Forum/ForumLess/ForumHomePage.less");
 </style>
